039130e629608613acbab2d58d7908f5bc3e126c
Tauqir <tauqirkhann.11@gmail.com>
2024-08-26 06:15:48 +0530

    feat: add renderBoardDOM func to render gameBoard

---
+++ src/functions/DOMfunctions/renderBoard.js
@@ -0,0 +1,20 @@
+import { gridValue } from "../utils";
+
+export const renderBoardDOM = (parentEl, board) => {
+  parentEl.innerHTML = "";
+  board.forEach((row, rowIndex) => {
+    row.forEach((grid, columnIndex) => {
+      const gridBtn = document.createElement("button");
+      gridBtn.classList.add("gridBtn");
+
+      gridBtn.dataset.row = rowIndex;
+      gridBtn.dataset.column = columnIndex;
+
+      let value = gridValue(grid);
+
+      gridBtn.textContent = value;
+
+      parentEl.appendChild(gridBtn);
+    });
+  });
+};
