682696bce0878e06007143d345c22709f4b0e191
Tauqir <tauqirkhann.11@gmail.com>
2024-09-02 06:00:32 +0530

    fix: player.js file to import correct module

--- src/functions/player.js
+++ src/functions/player.js
@@ -1,16 +1,13 @@
 import { Gameboard } from "./gameboard";
 
-export const Player = (
-  player1Name = "player1",
+export const Players = (
+  player1Name = "human",
   player2Name = "computer",
   player1 = Gameboard(),
   player2 = Gameboard()
 ) => {
   player1.name = player1Name;
   player2.name = player2Name;
-
-  console.log(player1);
-  console.log(player2);
 
   return {
     player1,
--- src/functions/test/player.test.js
+++ src/functions/test/player.test.js
@@ -1,7 +1,7 @@
-import { Player } from "../player";
+import { Players } from "../player";
 
 test("there are two players with thier own gameBoard", () => {
-  const player = Player("real player", "computer");
+  const player = Players("real player", "computer");
 
   expect(player.player1.name).toBe("real player");
   expect(player.player2.name).toBe("computer");
