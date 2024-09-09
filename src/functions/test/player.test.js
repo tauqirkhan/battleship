e5578c15ab0711463869a47de53028b29f1c713a
Tauqir <tauqirkhann.11@gmail.com>
2024-09-09 21:15:42 +0530

    Initial dist subtree commit

--- src/functions/test/player.test.js
+++
@@ -1,8 +0,0 @@
-import { Players } from "../player";
-
-test("there are two players with thier own gameBoard", () => {
-  const player = Players("real player", "computer");
-
-  expect(player.player1.name).toBe("real player");
-  expect(player.player2.name).toBe("computer");
-});
--- src/functions/test/ship.test.js
+++
@@ -1,9 +0,0 @@
-import { Ship } from "../ship";
-
-test("Battleship game ship", () => {
-  const ship = Ship(2);
-  expect(ship.isSunk()).toBe(false);
-  ship.hit();
-  ship.hit();
-  expect(ship.isSunk()).toBe(true);
-});
